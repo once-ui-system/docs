715ba83c562ca7222add7a5b739ea28034788baa
Lorant <lorant@once-ui.com>
2025-05-07 20:11:21 +0200

    structure

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -32,7 +32,7 @@ const style = {
   theme: "dark", // dark | light
   neutral: "gray", // sand | gray | slate
   brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   solid: "contrast", // color | contrast
   solidStyle: "flat", // flat | plastic
   border: "playful", // rounded | playful | conservative
